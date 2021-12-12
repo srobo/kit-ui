1cf22751cc4a52720ffd115802e2a9b06d405217
Karina Kwiatek <kjk@kjk.dog>
2021-12-12 18:50:44 +0100

    Redo all CSS/HTML using Bulma; add connecting modal

--- src/main.js
+++ src/main.js
@@ -1,46 +1,100 @@
+import mqtt from 'mqtt'
+
 const options = {
   keepalive: 30,
   clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
   protocolId: 'MQTT',
   protocolVersion: 4,
   clean: true,
-  reconnectPeriod: 1000,
-  connectTimeout: 30 * 1000,
+  reconnectPeriod: 0,
+  connectTimeout: 60 * 1000,
   rejectUnauthorized: false,
 }
 
 const client = mqtt.connect(`ws://${location.hostname}:9001`, options)
 const logMessageRegex = /\[(\d+:\d{2}:\d{2}\.\d+)] (.*)/
+let connectedServices = {
+  'astdiskd': false,
+  'astmetad': false,
+  'astprocd': false,
+}
 let $ = {}
+
+function updateServiceState() {
+  const runningServiceCount = Object.values(connectedServices).filter(val => val).length
+  if (runningServiceCount === Object.values(connectedServices).length) {
+    document.body.classList.add('is-connected')
+    document.getElementById('modal-disconnected').classList.remove('is-active')
+  } else {
+    document.getElementById('serviceProgress').value = runningServiceCount + 1
+  }
+}
 
 window.addEventListener('DOMContentLoaded', event => {
   $ = {
-    status: document.getElementById('status'),
     log: document.getElementById('log'),
     templates: {
       logEntry: document.getElementById('tpl-log-entry'),
     },
-    mode_select: document.getElementById('mode_select')
   }
+
+  document.querySelectorAll('[data-action]')
+    .forEach(el => el.addEventListener('click', function (e) {
+      e.preventDefault()
+      switch (e.target.dataset.action) {
+        case 'start':
+          broadcast('start_button')
+          break
+        case 'restart':
+          sendProcessRequest('restart')
+          break
+        case 'kill':
+          sendProcessRequest('kill')
+          break
+        case 'clearLog':
+          clearLog()
+          break
+      }
+    }))
+
+  document.querySelectorAll('.sends-mutate-request')
+    .forEach(el => el.addEventListener('change', function (e) {
+      sendMutateRequest(e.target.dataset.property, e.target.value)
+    }))
 })
 
 const status_labels = {
-  'code_crashed': '💣 Crashed',
-  'code_finished': '🏁 Finished',
-  'code_killed': '💀 Killed',
-  'code_running': '▶️ Running',
-  'code_starting': '⏩ Starting',
+  'code_crashed': 'Crashed',
+  'code_finished': 'Finished',
+  'code_killed': 'Killed',
+  'code_running': 'Running',
+  'code_starting': 'Starting',
 }
 
 client.on('connect', function () {
+  document.getElementById('serviceProgress').value = 1
   console.log('Connected!')
   client.subscribe('astoria/#')
 })
 
+const disconnected = function () {
+  document.getElementById('serviceProgress').removeAttribute('value')
+  document.body.classList.remove('is-connected')
+  document.getElementById('modal-disconnected').classList.add('is-active')
+  connectedServices = {
+    'astdiskd': false,
+    'astmetad': false,
+    'astprocd': false,
+  }
+}
+
 client.on('error', function (err) {
+  disconnected()
   console.error(err)
   client.end()
 })
+
+client.on('close', disconnected)
 
 const handlers = {
   'astoria/broadcast/usercode_log': contents => {
@@ -51,10 +105,14 @@ const handlers = {
     entryFragment.querySelector('.log-entry').dataset.source = contents.source
     entryFragment.querySelector('.log-entry__ts').textContent = ts
     const contentEl = entryFragment.querySelector('.log-entry__content')
-    contentEl.textContent = message
+    contentEl.innerHTML = message.replaceAll(' ', String.fromCharCode(0xA0))
 
-    if (message.indexOf('WARNING:') === 0) {
-      contentEl.classList.add('text-d-orange')
+    if (contents.source === 'astoria') {
+      contentEl.classList.add('has-text-weight-bold', 'has-text-centered', 'is-family-sans-serif')
+    } else if (contents.source === 'stderr') {
+      contentEl.classList.add('has-text-danger')
+    } else if (message.indexOf('WARNING:') === 0) {
+      contentEl.classList.add('has-text-warning')
     }
 
     $.log.appendChild(entryFragment)
@@ -63,14 +121,22 @@ const handlers = {
     createPlainLogEntry('▶️ Start button pressed', 'text-d-blue', 'text-bold')
   },
   'astoria/astdiskd': contents => {
+    connectedServices['astdiskd'] = contents.status === 'RUNNING'
+    updateServiceState()
     document.querySelectorAll('.controls button')
       .forEach(el => el.disabled = Object.values(contents.disks).filter(d => d.disk_type === 'USERCODE').length === 0)
   },
   'astoria/astmetad': contents => {
-    $.mode_select.value = contents.metadata.mode
+    connectedServices['astmetad'] = contents.status === 'RUNNING'
+    updateServiceState()
+    document.getElementById('mode_select').value = contents.metadata.mode
   },
   'astoria/astprocd': contents => {
-    $.status.textContent = status_labels[contents.code_status]
+    connectedServices['astprocd'] = contents.status === 'RUNNING'
+    updateServiceState()
+    const statusLabel = status_labels[contents.code_status]
+    document.getElementById('status').textContent = statusLabel
+    document.title = `Robot - ${statusLabel}`
   }
 }
 
@@ -93,20 +159,20 @@ client.on('message', function (topic, payload) {
 
 const isOwnPayload = contents => contents.hasOwnProperty('sender_name') && contents.sender_name === options.clientId
 
-function uuid4 () {
+function uuid4() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
     const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8)
     return v.toString(16)
   })
 }
 
-function createPlainLogEntry (text, ...classes) {
-  const entry = document.createElement('div')
-  entry.classList.add('log-entry')
-  for (const className of classes) {
-    entry.classList.add(className)
-  }
-  entry.textContent = text
+function createPlainLogEntry(text, ...classes) {
+  const entry = document.createElement('tr')
+  const entryContentElement = document.createElement('td')
+  entry.classList.add('plain-log-entry', ...classes)
+  entryContentElement.textContent = text
+  entryContentElement.setAttribute('colspan', 2)
+  entry.appendChild(entryContentElement)
   $.log.appendChild(entry)
 }
 
