9cf8087449a678a43b5907d17905f3ded097be35
James Seden Smith <sedders123@gmail.com>
2022-05-29 22:30:57 +0100

    👽 Use React 18 rendering method

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Routes as AppRoutes } from "constants/routes";
 import { LogViewer } from "components/LogViewer";
@@ -24,4 +24,6 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
