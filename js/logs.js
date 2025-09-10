699ecfea32acd22b26263926bd9661b0e43bb516
Karina Kwiatek <kkwiatek@studentrobotics.org>
2025-09-10 19:54:44 +0200

    Reformat

--- js/logs.js
+++ js/logs.js
@@ -72,7 +72,8 @@ export function createUsercodeLogEntry(contents) {
 
   $log.appendChild(entryFragment);
   generatedScrollEvent = true;
-  if (shouldAutoScroll) contentEl.parentElement.scrollIntoView({ block: "end" });
+  if (shouldAutoScroll)
+    contentEl.parentElement.scrollIntoView({ block: "end" });
 }
 
 export function initLog() {
